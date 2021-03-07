716f0c905d452a93b2d5acf6484c8019d5eb4a7a
Shankar4919 <itsme.shankar8@gmail.com>
2021-03-07 17:24:25 +0545

    applying_javascript_not_yet_finished

--- js/script.js
+++ js/script.js
@@ -2,3 +2,45 @@
 
 window.addEventListener('load',()=> document.querySelector('.preloader')
 .classList.add('hidePreloader'))
+
+// reveal pattern
+const CreateDogs = (()=>{
+    // dog data
+    const dogs = [];
+
+    // dog class
+    class Dog {
+        constructor(name, breed, img,special, price,type, gender, age){
+            this.name = name;
+            this.breed = breed;
+            this.img = img;
+            this.special = special;
+            this.price = price;
+            this.type = type;
+            this.gender = gender;
+            this.age = age;
+        }
+    }
+    // dog creation
+    function makeDog(name, breed, img='Img/husky_1.jpg',special=true, price=10000,type='new', gender='male', age='2'){
+        const dog = new Dog(name, breed, img,special=true, price,type, gender, age);
+        dogs.push(dog)
+    }
+    //produce dogs
+    function produceDogs(){
+        makeDog('tommy','german shaperd','Img/shepard_3.jpg')
+        makeDog('lucy', 'husky','Img/husky_1.jpg',true);
+        makeDog('pintu','bull dog', 'Img/bull_dog_1.jpg',false);
+        makeDog('pintu','bull dog', 'Img/bull_dog_1.jpg',false);
+        makeDog('pintu','bull dog', 'Img/bull_dog_1.jpg',false);
+        makeDog('pintu','bull dog', 'Img/bull_dog_1.jpg',false);
+    }
+    produceDogs();
+    console.log(dogs);
+
+    //special dogs
+    const specialDogs = dogs.filter(dog => dog.special === true)
+    console.log(specialDogs);
+
+
+})();
